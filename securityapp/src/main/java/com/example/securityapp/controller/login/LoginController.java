94ba1bd895d9dbe88c06d2dafbe968f0d5522d10
soominJung0413 <doli061214@gmail.com>
2020-09-28 01:38:03 +0900

    dev SecurityApp
    :)

--- securityapp/src/main/java/com/example/securityapp/controller/login/LoginController.java
+++ securityapp/src/main/java/com/example/securityapp/controller/login/LoginController.java
@@ -1,7 +1,13 @@
 package com.example.securityapp.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -9,5 +15,17 @@ public class LoginController {
     @GetMapping(value = "/login")
     public String login(){
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();// SecurityContextHolder 에서 Authentication 객체 추출
+
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);//Authentication 객체 존재시 SecurityContextLogoutHandler 가 session 과  authentication 객체 삭제
+        }
+
+        return "redirect:/";
     }
 }
